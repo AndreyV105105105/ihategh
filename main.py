0a7665e2d2684a830a0870fbc2d259a0a80649a1
Andrey <volvachandrey108@gmail.com>
2023-11-23 22:49:58 +0300

    com1(z3)

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 import sys
 import random
-from PyQt5 import uic
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
+from UI import Ui_MainWindow
 
-class Example(QMainWindow):
+
+def draw(qp):
+    for i in range(100):
+        qp.setBrush(QColor(random.randint(0, 255), random.randint(0, 255),
+                    random.randint(0, 255)))
+        x = random.randint(20, 600)
+        y = random.randint(20, 600)
+        r = random.randint(100, 100)
+        qp.drawEllipse(x, y, r, r)
+
+
+class Example(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        self.do_paint = None
         self.initUI()
+        self.do_paint = False
 
     def initUI(self):
-        uic.loadUi('UI.ui', self)
+        # uic.loadUi("UI.ui", self)
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.paint)
 
     def paint(self):
@@ -23,17 +35,9 @@ class Example(QMainWindow):
         if self.do_paint:
             qp = QPainter()
             qp.begin(self)
-            self.draw_flag(qp)
+            draw(qp)
             qp.end()
         self.do_paint = False
-
-    def draw_flag(self, qp):
-        qp.setBrush(QColor(255, 255, 0))
-        for i in range(100):
-            x = random.randint(20, 600)
-            y = random.randint(20, 600)
-            r = random.randint(10, 100)
-            qp.drawEllipse(x, y, r, r)
 
 
 if __name__ == '__main__':
